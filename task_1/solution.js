bff1e0b12bd8ae8c1928861cf0dc8616017a47ca
Gala <3550939dom@mail.ru>
2023-06-03 19:31:32 +0500

    выполгненое дз

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,6 +1,8 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message = productName + " за " + productPrice + " теперь в корзине!";
+    console.log("message", message);
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
@@ -23,6 +25,8 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = oldValue + 1;
+
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -36,6 +40,10 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+    let newSum = oldSum + difference;
+    let newSumText = `${newSum} ₽`;
+
+
 
     // Задание №1.3. Увеличение суммы в корзине
 
